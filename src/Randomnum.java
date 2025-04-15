88935eae75f129bcfa94974011152d794ae0cea0
xJvstvs <justusttv@gmail.com>
2025-04-15 18:50:27 +0200

    Randomnum.java updated / Fixxed a few Problems reguarding OOP

--- src/Randomnum.java
+++ src/Randomnum.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 
 public class Randomnum {
-    public static Integer number;
-    public static Integer rndmnum;
-    public static Integer i = 0;
-    public static void input() {
+    public Integer number;
+    public Integer rndmnum;
+    public Integer i;
+
+    public Randomnum() {
+        this.number = 0;
+        this.rndmnum = 0;
+        this.i = 0;
+    }
+
+    private void input() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a number between 1 and 100:");
         String input = scanner.nextLine();
@@ -16,12 +23,12 @@ public class Randomnum {
         }
     }
 
-    public static void generate() {
+    private void generate() {
+        i = 1;
         rndmnum = (int)(Math.random() * 100) + 1;
-        System.out.println("Random number generated: " + rndmnum);
     }
 
-    public static void equals(){
+    private void equals(){
         if (number < 1 || number > 100) {
             System.out.println("The number is out of range. Please enter a number between 1 and 100.");
             input();
@@ -42,11 +49,16 @@ public class Randomnum {
 
     }
 
-    public static void main(String[] args) {
+    public void play(){
         input();
         generate();
         equals();
         System.out.println("The number of attempts: " + i);
         System.out.println("The numbers are equal.");
+    }
+
+    public static void main(String[] args) {
+        Randomnum num = new Randomnum();
+        num.play();
     }
 }
