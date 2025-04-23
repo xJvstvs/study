4b04f6de1d4873ea6b4962dd70efd1d3c9982ca8
xJvstvs <justusttv@gmail.com>
2025-04-23 20:36:14 +0200

    Receipt finished / deletion complete / show list done / scratch created

--- src/Receipt.java
+++ src/Receipt.java
@@ -60,10 +60,10 @@ public class Receipt {
             JSONObject newCustomer = new JSONObject();
             newCustomer.put("adress", customerAdress);
             newCustomer.put("accountnr", customerAccountnr);
-            newCustomer.put("name", customerName);
-            JSONObject customerName = new JSONObject();
-            customerName.put(id + 1,newCustomer);
-            customerList.add(customerName);
+            newCustomer.put("name", customerName.toLowerCase());
+            JSONObject customerid = new JSONObject();
+            customerid.put(id + 1,newCustomer);
+            customerList.add(customerid);
             try (FileWriter writer = new FileWriter(jsonfile)) {
                 writer.write(customerList.toJSONString());
                 System.out.println("Customer added successfully:");
@@ -109,18 +109,112 @@ public class Receipt {
         } catch (IOException | ParseException e) {
             System.out.println("Error while retrieving the last ID: " + e.getMessage());
         }
-        System.out.println(lastID);
         return lastID;
     }
 
 
-    private void start() throws IOException, ParseException {
-        fileImplementation();
-        id = getLastID();
-        addCustomer();
-
+    private void getCustomer() throws IOException, ParseException {
+        System.out.println("Whats the name of the Customer you want to get");
+        String customernamelocal = scanner.nextLine().toLowerCase();
+        JSONParser parser = new JSONParser();
+        JSONArray customerList = (JSONArray) parser.parse(new FileReader(jsonfile));
+        for (Object obj : customerList) {
+            JSONObject customerEntry = (JSONObject) obj;
+            for (Object key : customerEntry.keySet()) { // Zugriff auf die ID
+                JSONObject customerData = (JSONObject) customerEntry.get(key); // Inneres JSONObject
+                String customern = (String) customerData.get("name"); // Zugriff auf den Namen
+                if (customern.equals(customernamelocal)) {
+                    System.out.println("---------------------------------");
+                    System.out.println("Name: " + customerData.get("name"));
+                    System.out.println("---> Adress: " + customerData.get("adress"));
+                    System.out.println("---> Accountnumber: " + customerData.get("accountnr"));
+                    System.out.println("---> ID: " + (customerEntry.keySet()));
+                    System.out.println("---------------------------------");
+                }
+            }
+        }
     }
 
+    private void removeCustomer() throws IOException, ParseException {
+        System.out.println("Whats the name of the Customer you want to remove?");
+        String customernamelocal = scanner.nextLine().toLowerCase();
+        JSONParser parser = new JSONParser();
+        JSONArray customerList = (JSONArray) parser.parse(new FileReader(jsonfile));
+        boolean customerFound = false;
+        for (int i = 0; i < customerList.size(); i++) {
+            JSONObject customerEntry = (JSONObject) customerList.get(i);
+            for (Object key : customerEntry.keySet()) { // Access the ID
+                JSONObject customerData = (JSONObject) customerEntry.get(key); // Inner JSONObject
+                String customern = (String) customerData.get("name"); // Access the name
+                if (customern.equals(customernamelocal)) {
+                    customerList.remove(i); // Remove the customer entry from the array
+                    customerFound = true;
+                    System.out.println("Customer removed successfully.");
+                    break;
+                }
+            }
+            if (customerFound) break; // Exit the loop once the customer is removed
+        }
+        try (FileWriter writer = new FileWriter(jsonfile)) {
+            writer.write(customerList.toJSONString());
+        }
+    }
+
+    private void showList() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        JSONArray customerList = (JSONArray) parser.parse(new FileReader(jsonfile));
+        for (Object obj : customerList) {
+            JSONObject customerEntry = (JSONObject) obj;
+            for (Object key : customerEntry.keySet()) { // Zugriff auf die ID
+                JSONObject customerData = (JSONObject) customerEntry.get(key); // Inneres JSONObject
+                System.out.println("---------------------------------");
+                System.out.println("Name: " + customerData.get("name"));
+                System.out.println("---> Adress: " + customerData.get("adress"));
+                System.out.println("---> Accountnumber: " + customerData.get("accountnr"));
+                System.out.println("---> ID: " + (customerEntry.keySet()));
+            }
+        }
+    }
+
+    private void start() throws IOException, ParseException {
+        fileImplementation();
+        while (true) {
+            System.out.println();
+            System.out.println("What do you want to do? 1 = add Customer, 2 = get Customer, 3 = remove Customer, 4 = Show list, 5 = Exit");
+            try {
+                int num = Integer.parseInt(scanner.nextLine());
+                switch (num) {
+                    case 1: {
+                        id = getLastID();
+                        addCustomer();
+                        break;
+                    }
+                    case 2: {
+                        getCustomer();
+                        break;
+                    }
+                    case 3: {
+                        removeCustomer();
+                        break;
+                    }
+                    case 4:{
+                        showList();
+                        break;
+                    }
+                    case 5: {
+                        System.out.println("Exit, goodbye");
+                        return;
+                    }
+                    default: {
+                        System.out.println("This is not a valid number, please restart again");
+                        break;
+                    }
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException, ParseException {
         Receipt receipt = new Receipt();
